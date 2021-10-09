2d7403d6db6caf7ff7ad25f6cadbe6a2699344c8
Sebastian Maciejewski <maciejewski.torun@gmail.com>
2021-10-09 19:35:53 +0200

    Update test from iPad editor

--- server/tests/engine.test.ts
+++ server/tests/engine.test.ts
@@ -1,6 +1,21 @@
+import { GameState } from './src/GameState'
+import { UserSession } from './src/model'
+import { Engine } from './src/Engine'
+
 describe('game engine tests', () => {
-    test('user session', () => {
+
+	const engine = new Engine()
+	const userSession: UserSession
+	beforeEach(() => {
+		userSession = {
+			sessionId: '1',
+			state: new GameState()
+	})
+
+    test('generate quiz data', async () => {
+    	userSession = await engine.generateQuizData(userSession)
         // TODO: engine test with mocked UserSession
-        expect(true).toBeTruthy()
+        epxect(userSession.state.quizData).toBeTruthy()
+        // expect(true).toBeTruthy()
     })
 })
