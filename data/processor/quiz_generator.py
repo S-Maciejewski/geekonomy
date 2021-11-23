eec38248099c73f707156334850a3127ac53a80e
S-Maciejewski <maciejewski.torun@gmail.com>
2021-11-23 18:16:37 +0100

    feat: saving pre-generated quiz instances

--- data/processor/quiz_generator.py
+++ data/processor/quiz_generator.py
@@ -1,19 +1,20 @@
 import csv
 import random
 import json
+import time
+
 import psycopg2
 from typing import List
 
 from config import config
 
+QUIZ_DIRECTORY_PATH = '../../quiz_data'
+
 
 class QuizGenerator:
-    def __init__(self):
+    def __init__(self, skip_nulls: bool = False):
+        self.skip_nulls = skip_nulls
         self.connection = psycopg2.connect(**config())
-        # with open('./supported_countries.csv', newline='') as file:
-        #     reader = csv.reader(file)
-        #     self.supported_countries = [item[0] for item in list(reader)]
-
         with open('./supported_countries_weights.csv', newline='') as file:
             reader = csv.reader(file)
             self.supported_countries_weights = [[item[0], item[1]] for item in list(reader)]
@@ -25,21 +26,21 @@ class QuizGenerator:
     def __del__(self):
         self.connection.close()
 
-    def get_weighted_random_countries(self, k=4):
-        # TODO: random.choices is very quick but can draw duplicates - they should be filtered out or sampling method should be changed
-        return random.choices(
-            population=[item[0] for item in self.supported_countries_weights],
-            weights=[float(item[1]) for item in self.supported_countries_weights],
-            k=k
-        )
+    def get_weighted_random_countries(self, k: int = 4):
+        # random.choices is very quick but can draw duplicates - they should be filtered out in a fast way
+        choices = []
+        while len(list(set(choices))) != k:
+            choices = random.choices(population=[item[0] for item in self.supported_countries_weights],
+                                     weights=[float(item[1]) for item in self.supported_countries_weights], k=k)
+        return choices
 
-    def get_random_indicators(self, k=4):
+    def get_random_indicators(self, k: int = 4):
         return random.choices(
             population=self.supported_indicators,
             k=k
         )
 
-    def get_single_quiz(self, countries: List[str], indicators: List[str], skip_nulls: bool = True):
+    def get_single_quiz(self, countries: List[str], indicators: List[str]):
         cursor = self.connection.cursor()
         countries_formatted = "'" + "','".join(countries) + "'"
         indicators_formatted = "'" + "','".join(indicators) + "'"
@@ -51,8 +52,8 @@ class QuizGenerator:
         cursor.execute(query)
         query_result = cursor.fetchall()
 
-        def get_series(country, indicator):
-            if skip_nulls:
+        def get_series(country: str, indicator: str):
+            if self.skip_nulls:
                 return list(map(lambda x: [int(x[2]), float(x[3]) if x[3] is not None else None],
                                 filter(lambda x: x[0] == indicator and x[1] == country and x[3] is not None,
                                        query_result)))
@@ -72,11 +73,23 @@ class QuizGenerator:
         # TODO: validate quiz completeness, repeat the random generation if indicators insufficiently filled
         return quiz
 
+    def generate_quiz_files(self, n: int = 10):
+        start_time = time.time()
+        for i in range(n):
+            quiz = self.get_single_quiz(self.get_weighted_random_countries(), self.get_random_indicators())
+            with open(f'{QUIZ_DIRECTORY_PATH}/quiz_{i}.json', 'w') as file:
+                json.dump(quiz, file)
+                print(f'Quiz {i + 1} of {n} generated in {time.time() - start_time} seconds')
+                file.close()
+
 
 if __name__ == '__main__':
     quiz_generator = QuizGenerator()
-    quiz = quiz_generator.get_single_quiz(quiz_generator.get_weighted_random_countries(),
-                                          quiz_generator.get_random_indicators())
-    print(quiz)
-    with open('quiz_without_nulls.json', 'w+') as file:
-        json.dump(quiz, file)
+
+    quiz_generator.generate_quiz_files(10)
+
+    # quiz = quiz_generator.get_single_quiz(quiz_generator.get_weighted_random_countries(),
+    #                                       quiz_generator.get_random_indicators())
+    # print(quiz)
+    # with open('quiz_without_nulls.json', 'w+') as file:
+    #     json.dump(quiz, file)
