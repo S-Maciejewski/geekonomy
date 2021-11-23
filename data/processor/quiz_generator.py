43d51769731f3d373f87ff490a640ffe65e46cf8
S-Maciejewski <maciejewski.torun@gmail.com>
2021-11-23 22:32:31 +0100

    Generator cleanup, query validation approaches, random correct country fix

--- data/processor/quiz_generator.py
+++ data/processor/quiz_generator.py
@@ -11,6 +11,17 @@ from config import config
 QUIZ_DIRECTORY_PATH = '../../quiz_data'
 
 
+def get_dwh_query_result(cursor, countries: List[str], indicators: List[str]):
+    countries_formatted = "'" + "','".join(countries) + "'"
+    indicators_formatted = "'" + "','".join(indicators) + "'"
+
+    query = f'select "Indicator Code", "Country Code", "Year", "Value" from "Data"' \
+            f'where "Country Code" in ({countries_formatted}) and "Indicator Code" in ({indicators_formatted})' \
+            f'order by "Country Code", "Indicator Code", "Year"'
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
 class QuizGenerator:
     def __init__(self, skip_nulls: bool = False):
         self.skip_nulls = skip_nulls
@@ -34,32 +45,27 @@ class QuizGenerator:
                                      weights=[float(item[1]) for item in self.supported_countries_weights], k=k)
         return choices
 
-    def get_random_indicators(self, k: int = 4):
+    def get_random_indicators(self, skipped_indicators: List[str] = [], k: int = 4):
+        population = self.supported_indicators if skipped_indicators == [] else \
+            [x for x in self.supported_indicators if x not in skipped_indicators]
         return random.choices(
-            population=self.supported_indicators,
+            population=population,
             k=k
         )
 
+    def parse_series(self, query_result, country: str, indicator: str):
+        if self.skip_nulls:
+            return list(map(lambda x: [int(x[2]), float(x[3]) if x[3] is not None else None],
+                            filter(lambda x: x[0] == indicator and x[1] == country and x[3] is not None,
+                                   query_result)))
+        return list(map(lambda x: [int(x[2]), float(x[3]) if x[3] is not None else None],
+                        filter(lambda x: x[0] == indicator and x[1] == country,
+                               query_result)))
+
     def get_single_quiz(self, countries: List[str], indicators: List[str]):
         cursor = self.connection.cursor()
-        countries_formatted = "'" + "','".join(countries) + "'"
-        indicators_formatted = "'" + "','".join(indicators) + "'"
 
-        query = f'select "Indicator Code", "Country Code", "Year", "Value" from "Data"' \
-                f'where "Country Code" in ({countries_formatted}) and "Indicator Code" in ({indicators_formatted})' \
-                f'order by "Country Code", "Indicator Code", "Year"'
-
-        cursor.execute(query)
-        query_result = cursor.fetchall()
-
-        def get_series(country: str, indicator: str):
-            if self.skip_nulls:
-                return list(map(lambda x: [int(x[2]), float(x[3]) if x[3] is not None else None],
-                                filter(lambda x: x[0] == indicator and x[1] == country and x[3] is not None,
-                                       query_result)))
-            return list(map(lambda x: [int(x[2]), float(x[3]) if x[3] is not None else None],
-                            filter(lambda x: x[0] == indicator and x[1] == country,
-                                   query_result)))
+        query_result = get_dwh_query_result(cursor, countries, indicators)
 
         indicators_data = []
         for country in countries:
@@ -67,10 +73,16 @@ class QuizGenerator:
                 indicators_data.append({
                     'indicator': indicator,
                     'country': country,
-                    'series': get_series(country, indicator)})
+                    'series': self.parse_series(query_result, country, indicator)})
 
-        quiz = {'countries': countries, 'indicators': indicators_data, 'correctCountry': countries[0]}
+        quiz = {'countries': countries, 'indicators': indicators_data, 'correctCountry': random.choice(countries)}
         # TODO: validate quiz completeness, repeat the random generation if indicators insufficiently filled
+        # Two approaches:
+        # 1. Heuristic: if correctCountry has an indicator with no (or little) data, repeat the generation with this indicator skipped
+        #    same goes for indicators that have i.e. less than 50% of data for all countries combined -> heavy on generator
+        # 2. Query the DWH beforehand: after getting the random country list and correct country, query the DWH for a list
+        #    of indicators that are sufficiently filled for correct country and other countries. -> heavy on DWH
+
         return quiz
 
     def generate_quiz_files(self, n: int = 10):
