179e69da86a2fb3b72e063988f03b4b69324ddfb
S-Maciejewski <maciejewski.torun@gmail.com>
2022-10-28 09:30:13 +0200

    fix: log file name without ':' so that it works on Windows

--- server/src/Logger.ts
+++ server/src/Logger.ts
@@ -1,23 +1,21 @@
 import pino from 'pino';
 
-// TODO: add multistream support (file, stdout)
-
-// TODO: fix problems with pino not being able to create files
-// const logger = pino({}, pino.destination(`./logs/server_${new Date().toISOString().slice(0, 19).replace('T', '-')}.log`));
+const logFilePath = `./logs/server_${Date.now()}.log`;
+const logger = pino({}, pino.destination(logFilePath));
 
 export namespace Logger {
     export const info = (msg: string, ...args: any[]) => {
-        // logger.info({msg, args})
+        logger.info({msg, args})
         console.log(msg, ...args)
     }
 
     export const error = (msg: string, ...args: any[]) => {
-        // logger.error({msg, args})
+        logger.error({msg, args})
         console.log(msg, ...args)
     }
 
     export const warn = (msg: string, ...args: any[]) => {
-        // logger.warn({msg, args})
+        logger.warn({msg, args})
         console.log(msg, ...args)
     }
 }
