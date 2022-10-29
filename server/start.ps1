d0869517192bcb5d249800538c3a79befc5a23c7
S-Maciejewski <maciejewski.torun@gmail.com>
2022-10-29 19:02:00 +0200

    feat: docker-compose configuration with nginx

--- server/start.ps1
+++ server/start.ps1
@@ -4,7 +4,8 @@ docker build -t server .
 docker run --name server -d `
 -e QUIZ_DATA_PATH='/quiz_data' `
 -e SERVER_ADDRESS='0.0.0.0' `
+-e PORT=80 `
 -v C:\Projekty\geekonomy\quiz_data:/quiz_data `
--p 8080:8080 `
+-p 80:80 `
 --rm `
 server:latest
--- start.ps1
+++ start.ps1
@@ -1,10 +1,11 @@
-docker rm -f nginx-proxy nginx-proxy-letsencrypt
+# docker rm -f nginx-proxy nginx-proxy-letsencrypt
 docker-compose down
 
-docker run --detach --name nginx-proxy --publish 80:80 --publish 443:443 --volume /etc/nginx/certs --volume /etc/nginx/vhost.d --volume /usr/share/nginx/html --volume /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+# docker run --detach --name nginx-proxy --publish 80:80 --publish 443:443 --volume /etc/nginx/certs --volume /etc/nginx/vhost.d --volume /usr/share/nginx/html --volume /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 
-docker run --detach --name nginx-proxy-letsencrypt --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
+# docker run --detach --name nginx-proxy-letsencrypt --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
 
+# docker-compose up -d --build
 docker-compose up -d --build
 
-docker network connect geekonomy_default nginx-proxy
+# docker network connect geekonomy_default nginx-proxy
