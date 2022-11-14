afe64dc349b52e697e1c8eb3b7c0cc2dddc68b35
S-Maciejewski <maciejewski.torun@gmail.com>
2022-11-14 23:09:15 +0100

    feat: ReactGA and dependency updates

--- client/data-quiz/src/index.js
+++ client/data-quiz/src/index.js
@@ -1,3 +1,4 @@
+import ReactGA from 'react-ga';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,11 +6,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n/i18'
 
+ReactGA.initialize('G-3KHM4P54HV');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
